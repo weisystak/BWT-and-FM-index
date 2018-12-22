472c7d673c9327a5588abe0a5abab6890101101b
yijia1127cs06g <yijia1127.cs06g@nctu.edu.tw>
2018-12-22 22:02:39 +0800

    add openMP flag to CMakeLists.txt 	parallel Aligner.hpp

--- Aligner.hpp
+++ Aligner.hpp
@@ -1,8 +1,10 @@
 #pragma once
+#include <omp.h>
 #include "suffix_array/DC3.hpp"
 #define k 256   // number of characters
 #define z 15     // tally table factor
 #define e 5     // SA factor
+
 
 class Aligner
 {
@@ -18,6 +20,7 @@ class Aligner
         assert(str.length() != 0);
         
         vector<int> s(str.length());
+        # pragma omp parallel for
         for(int i = 0; i<str.length(); i++)
             s[i] = str[i];
         
@@ -48,7 +51,8 @@ class Aligner
         // build tally table
         int tally_len = 1 + L.length()/z;
         if(L.length() % z == 0) tally_len--;
-
+        
+        # pragma omp parallel for
         for(int j = 0; j < 256; j++)
         {
             tally[j].resize( tally_len );
@@ -59,7 +63,6 @@ class Aligner
         }
 
         tally[ L[0] ][0]++;
-
 
         for(int i = 1, ii = 1; i < L.length(); i++)
         {
@@ -75,6 +78,7 @@ class Aligner
         }
         
         // init F
+        # pragma omp parallel for
         for(int i = 0; i<256; i++)
         {
             F[i] = 0;
@@ -113,6 +117,7 @@ class Aligner
     {
         int r = i%z, off = 0;
         int base = i-r+1;
+        # pragma omp parallel for
         for(int j = 0; j < r; j++)
             if( ch == L[ base+j ])
                 off++;
@@ -170,6 +175,7 @@ class Aligner
         else if( I == 0)
         {
             vector<int> res;
+            
             for(int i = a; i < b; i++)
                 res.push_back(deduce_idx(i));
             
